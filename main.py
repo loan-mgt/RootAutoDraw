bb08564fd609e3b79bb440a2fd427d5f125b6296
doantientai <doantientai@gmail.com>
2022-04-08 23:22:12 +0200

    flexible input sizes

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from PIL import Image
 import h5py
 import numpy as np
 
-#Keras
+# Keras
 from keras.models import model_from_json
-from keras.models import Model
 
 sys.path.insert(0, './lib/')
 from help_functions import load_hdf5, write_hdf5, rgb2gray, group_images, visualize, masks_Unet, pred_to_imgs
-from extract_patches import recompone, recompone_overlap, paint_border, paint_border_overlap, extract_ordered_overlap, kill_border, pred_not_only_FOV, get_data_testing, get_data_testing_overlap
+from extract_patches import recompone, recompone_overlap, paint_border, paint_border_overlap, extract_ordered_overlap, \
+    kill_border, pred_not_only_FOV, get_data_testing, get_data_testing_overlap
 from pre_processing import my_PreProc
 
 PATH_INPUT = "input/"
@@ -25,112 +25,86 @@ architechture_path = "model/architecture.json"
 
 os.makedirs("output", exist_ok=True)
 
-def write_hdf5(arr,outfile):
-    with h5py.File(outfile,"w") as f:
+
+def write_hdf5(arr, outfile):
+    with h5py.File(outfile, "w") as f:
         f.create_dataset("image", data=arr, dtype=arr.dtype)
 
+
 def get_datasets(imgs_dir):
-    
     Nimgs = len([f for f in listdir(imgs_dir) if isfile(join(imgs_dir, f))])
-    imgs = np.empty((Nimgs,height,width,channels))
-    for path, subdirs, files in os.walk(imgs_dir): #list all files, directories in the path
+    imgs = np.empty((Nimgs, height, width, channels))
+    for path, subdirs, files in os.walk(imgs_dir):  # list all files, directories in the path
         for i in range(len(files)):
-            print ("image: " +files[i])
-            
-            ### write some lines of code to:
-            ### - check if image's size is different to requirement, then resize and save to output folder
-            ### - else: make a copy to output folder
-            
-            img = Image.open(imgs_dir+files[i])
-            imgs[i] = np.asarray(img)    
-    imgs = np.transpose(imgs,(0,3,1,2))
-    assert(imgs.shape == (Nimgs,channels,height,width))
+            print("image: " + files[i])
+            img = Image.open(imgs_dir + files[i])
+            imgs[i] = np.asarray(img)
+    imgs = np.transpose(imgs, (0, 3, 1, 2))
+    assert (imgs.shape == (Nimgs, channels, height, width))
     return imgs, files
+
+
+def get_image(image_pil):
+    image_as_array = np.asarray(image_pil)
+    image_as_array = np.expand_dims(image_as_array, 0)
+    image_as_array = np.transpose(image_as_array, (0, 3, 1, 2))
+    print(image_as_array.shape)
+    return image_as_array
+
 
 def get_data(imgs_test, patch_height, patch_width, stride_height, stride_width):
     test_imgs_original = imgs_test
     test_imgs = my_PreProc(test_imgs_original)
-    test_imgs = test_imgs[:,:,:,:]
+    test_imgs = test_imgs[:, :, :, :]
     test_imgs = paint_border_overlap(test_imgs, patch_height, patch_width, stride_height, stride_width)
-
-#     print "\ntest images shape:"
-#     print test_imgs.shape
-#     print "range (min-max): " +str(np.min(test_imgs)) +' - '+str(np.max(test_imgs))
-   
-    #extract the TEST patches from the full images
-    patches_imgs_test = extract_ordered_overlap(test_imgs,patch_height,patch_width,stride_height,stride_width)
-
-#     print "\ntest PATCHES images shape:"
-#     print patches_imgs_test.shape
-#     print "range (min-max): " +str(np.min(patches_imgs_test)) +' - '+str(np.max(patches_imgs_test))
-    
+    # extract the TEST patches from the full images
+    patches_imgs_test = extract_ordered_overlap(test_imgs, patch_height, patch_width, stride_height, stride_width)
     return patches_imgs_test, test_imgs.shape[2], test_imgs.shape[3]
-  
+
+
 def createDir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
     else:
-        print (directory + " already exists")
+        print(directory + " already exists")
 
-channels = 3
-height = 2336
-width = 1696
 
-# dimension of the patches
-patch_height = 32
-patch_width = 32
+if __name__ == '__main__':
+    # Load model
+    model = model_from_json(open(architechture_path).read())
+    model.load_weights(model_path)
+    
+    for filename in listdir("input"):
+        if filename == ".ipynb_checkpoints":
+            continue
+        path_image = join("input", filename)
+        image = Image.open(path_image)
+        width, height = image.size
+        channels = 3
 
-#the stride in case output with average
-stride_height = 20
-stride_width = 20
-assert (stride_height < patch_height and stride_width < patch_width)
+        # dimension of the patches
+        patch_size = 32
+        stride = 20
 
-#load testing datasets
-imgs_test, file_names = get_datasets(original_imgs_test)
-print ("test datasets loaded")
-print(imgs_test.shape)
+        image_array = get_image(image)
 
-#original test images
-test_imgs_orig = imgs_test
-full_img_height = test_imgs_orig.shape[2]
-full_img_width = test_imgs_orig.shape[3]
+        patches_imgs_test, new_height, new_width = get_data(
+            imgs_test=image_array,
+            patch_height=patch_size,
+            patch_width=patch_size,
+            stride_height=stride,
+            stride_width=stride
+        )
 
-#Images to patches:
-patches_imgs_test = None
-new_height = None
-new_width = None
-masks_test  = None
-patches_masks_test = None
+        # Calculate the predictions
+        predictions = model.predict(patches_imgs_test, batch_size=32, verbose=2)
 
-patches_imgs_test, new_height, new_width = get_data(
-    imgs_test = imgs_test,
-    patch_height = patch_height,
-    patch_width = patch_width,
-    stride_height = stride_height,
-    stride_width = stride_width
-)
-
-#Load model
-model = model_from_json(open(architechture_path).read())
-model.load_weights(model_path)
-
-#Calculate the predictions
-predictions = model.predict(patches_imgs_test, batch_size=32, verbose=2)
-
-#===== Convert the prediction arrays in corresponding images
-pred_patches = pred_to_imgs(predictions, patch_height, patch_width, "original")
-pred_imgs = None
-pred_imgs = recompone_overlap(pred_patches, new_height, new_width, stride_height, stride_width)# predictions
-pred_imgs = pred_imgs[:,:,0:full_img_height,0:full_img_width]
-
-assert(len(file_names) == pred_imgs.shape[0])
-
-N_predicted = pred_imgs.shape[0]
-
-# Save predictions to files
-for i in range(int(N_predicted)):
-    pred_stripe = group_images(pred_imgs[i:i+1,:,:,:],1)
-    file_name =  file_names[i]
-    visualize(pred_stripe, "output/" + file_name[0:len(file_name)-4] + "_pred")
-
-print ("All done! Please check the output folder")
+        # ===== Convert the prediction arrays in corresponding images
+        pred_patches = pred_to_imgs(predictions, patch_size, patch_size, "original")
+        pred_imgs = None
+        pred_imgs = recompone_overlap(pred_patches, new_height, new_width, stride, stride)  # predictions
+        pred_imgs = pred_imgs[:, :, 0:height, 0:width]
+        pred_stripe = group_images(pred_imgs[:, :, :, :], 1)
+        visualize(pred_stripe, "output/" + filename[0:-4] + "_pred")
+        
+    print("All done! Please check the output folder")
